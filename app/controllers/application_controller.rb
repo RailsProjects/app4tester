d10cd0e9bc9e120fde8d0f1bfd674b1ade717672
Laurence G. Kauffman <lgkauffman@yahoo.com>
2013-10-08 23:46:08 -0700

    message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,14 @@ class ApplicationController < ActionController::Base
   private
 
   def mobile_device?
-    if session[:mobile_param]  # if session exists then
-      session[:mobile_param] == "1" #  rtn that session if mobile param  == 1 (it's a mobile device)
-    else
-  	  request.user_agent =~ /Mobile|webOS/  # else chk user agent string against regex
-    end
+      request.user_agent =~ /Mobile|webOS/  # else chk user agent string against regex
+
+
+    # if session[:mobile_param]  # if session exists then
+    #   session[:mobile_param] == "1" #  rtn that session if mobile param  == 1 (it's a mobile device)
+    # else
+  	 #  request.user_agent =~ /Mobile|webOS/  # else chk user agent string against regex
+    # end
   end
 
   helper_method :mobile_device?
@@ -26,6 +29,6 @@ class ApplicationController < ActionController::Base
   def prepare_for_mobile
     # set mobile param (passed thru URL) inside of session if it exists
     session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
+    # request.format = :mobile if mobile_device?
   end
 end
